a268432d13cff6b3381fd648a5faf4cab7b6605d
jungmin <go.jminn@gmail.com>
2020-12-05 23:10:07 +0900

    #4. 회원 정보 수정 페이지

---
+++ member_modify_form.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html>
+<head> 
+<meta charset="utf-8">
+<title>PHP 프로그래밍 입문</title>
+<link rel="stylesheet" type="text/css" href="./css/common.css">
+<link rel="stylesheet" type="text/css" href="./css/member.css">
+<script type="text/javascript" src="./js/member_modify.js"></script>
+</head>
+<body> 
+	<header>
+    	<?php include "header.php";?>
+    </header>
+<?php
+    $con = mysqli_connect("localhost", "copycat", "wotn0154!", "copy");
+    $sql = "select * from members where id='$id'";
+    $result = mysqli_query($con, $sql);
+    $row = mysqli_fetch_array($result);
+    $pass = $row["pass"];
+    $name = $row["name"];
+    $email = explode("@", $row["email"]); //explode(“@”,$row[“email”]) 은 을 기준으로 $row[“ 을 분리하여 배열에 저장
+    $email1 = $email[0];
+    $email2 = $email[1];
+    mysqli_close($con);
+?>
+    <section>
+		<div id="main_img_bar">
+            <img src="./img/main_img.png">
+        </div>
+        <div id="main_content">
+      		<div id="join_box">
+          	<form  name="member_form" method="post" action="member_modify.php?id=<?=$userid?>"> <!--저장하기 버튼을 눌렀을 때 아무이상 없으면 member_modify.php파일로 이동-->
+			    <h2>회원 정보수정</h2>
+    		    	<div class="form id">
+				        <div class="col1">아이디</div>
+				        <div class="col2">
+							<?=$userid?>
+				        </div>                 
+			       	</div>
+			       	<div class="clear"></div>
+
+			       	<div class="form">
+				        <div class="col1">비밀번호</div>
+				        <div class="col2">
+							<input type="password" name="pass" value="<?=$pass?>">
+				        </div>                 
+			       	</div>
+			       	<div class="clear"></div>
+			       	<div class="form">
+				        <div class="col1">비밀번호 확인</div>
+				        <div class="col2">
+							<input type="password" name="pass_confirm" value="<?=$pass?>">
+				        </div>                 
+			       	</div>
+			       	<div class="clear"></div>
+			       	<div class="form">
+				        <div class="col1">이름</div>
+				        <div class="col2">
+							<input type="text" name="name" value="<?=$name?>">
+				        </div>                 
+			       	</div>
+			       	<div class="clear"></div>
+			       	<div class="form email">
+				        <div class="col1">이메일</div>
+				        <div class="col2">
+							<input type="text" name="email1" value="<?=$email1?>">@<input 
+							       type="text" name="email2" value="<?=$email2?>">
+				        </div>                 
+			       	</div>
+			       	<div class="clear"></div>
+			       	<div class="bottom_line"> </div>
+			       	<div class="buttons">
+	                	<img style="cursor:pointer" src="./img/button_save.gif" onclick="check_input()">&nbsp;
+                  		<!--저장하기 버튼을 누르면 onclick에 의해 member_modify.js의 check_input()함수가 실행 -->
+                        <img id="reset_button" style="cursor:pointer" src="./img/button_reset.gif"
+                  			onclick="reset_form()">
+	           		</div>
+           	</form>
+        	</div> <!-- join_box -->
+        </div> <!-- main_content -->
+	</section> 
+	<footer>
+    	<?php include "footer.php";?>
+    </footer>
+</body>
+</html>
