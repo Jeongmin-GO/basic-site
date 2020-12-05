2e5a9e1b159c5ad689bbed76c2796cd400fd5f02
jungmin <go.jminn@gmail.com>
2020-12-06 02:20:58 +0900

    회원 db저장 수정

--- member_insert.php
+++ member_insert.php
@@ -1,23 +1,27 @@
 <?php
-    $id = $_POST["id"];
+    $id   = $_POST["id"];
     $pass = $_POST["pass"];
     $name = $_POST["name"];
-    $email1 = $_POST["email1"];
-    $email2 = $_POST["email2"];
+    $email1  = $_POST["email1"];
+    $email2  = $_POST["email2"];
 
-    $email = $email1. "@" .$email2;
-    $regist_day = date("Y-m-d (H:i)");
+    $email = $email1."@".$email2;
+    $regist_day = date("Y-m-d (H:i)");  // 현재의 '년-월-일-시-분'을 저장
 
+              
     $con = mysqli_connect("localhost", "copycat", "wotn0154!", "copy");
-    $sql = "insert into members(id, pass, name, email, regist_day, level, point) ";
-    $sql .= "values('$id', '$pass', '$name', '$email', '$regist_day', 9, 0)"; //입력받은 정보 db에 저장
-    
-    mysqli_query($con, $sql);
-    mysqli_close($con);
+
+	$sql = "insert into members(id, pass, name, email, regist_day, level, point) ";
+	$sql .= "values('$id', '$pass', '$name', '$email', '$regist_day', 9, 0)";
+
+	mysqli_query($con, $sql);  // $sql 에 저장된 명령 실행
+    mysqli_close($con);     
 
     echo "
-    <script>
-        location.href = 'index.php';
-    </script>
-    ";
+	      <script>
+	          location.href = 'index.php';
+	      </script>
+	  ";
 ?>
+
+   
